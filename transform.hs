fc69357a24b32a3353033d1b649d93ed20482c0a
Dag Odenhall <dag.odenhall@gmail.com>
2011-12-07 16:34:59 +0100

    remove nav tables, fixes #5

--- transform.hs
+++ transform.hs
@@ -3,6 +3,9 @@ import Text.Pandoc
 transform (CodeBlock _ code)
   = RawBlock "mediawiki" $ "<haskell>\n" ++ code ++ "\n</haskell>"
 
+transform (Table [] [AlignLeft,AlignLeft,AlignLeft] [0.0,0.0,0.0] [] _)
+  = Null
+
 transform x = x
 
 main = interact $ writeNative defaultWriterOptions
